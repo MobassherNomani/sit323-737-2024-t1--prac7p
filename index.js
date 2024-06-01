c48b4478b13482be1c147c498d4fed77c52e2c0c
MobassherNomani <mobassher.nomanibub@gmail.com>
2024-06-01 17:35:35 +1000

    2nd commit

--- index.js
+++ index.js
@@ -27,24 +27,24 @@ const logger = winston.createLogger({
     }));
   }
   //add
-const add= (n1,n2) => {
-    return n1+n2;
+const add= (num1,num2) => {
+    return num1+num2;
 }
 app.get("/add", (req,res)=>{
     try{
-    const n1= parseFloat(req.query.n1);
-    const n2=parseFloat(req.query.n2);
-    if(isNaN(n1)) {
-        logger.error("n1 is incorrectly defined");
-        throw new Error("n1 incorrectly defined");
+    const num1= parseFloat(req.query.num1);
+    const num2=parseFloat(req.query.num2);
+    if(isNaN(num1)) {
+        logger.error("num1 is incorrectly defined");
+        throw new Error("num1 incorrectly defined");
     }
-    if(isNaN(n2)) {
-        logger.error("n2 is incorrectly defined");
-        throw new Error("n2 incorrectly defined");
+    if(isNaN(num2)) {
+        logger.error("num2 is incorrectly defined");
+        throw new Error("num2 incorrectly defined");
     }
     
-    logger.info('Parameters '+n1+' and '+n2+' received for addition');
-    const result = add(n1,n2);
+    logger.info('Parameters '+num1+' and '+num2+' received for addition');
+    const result = add(num1,num2);
     res.status(200).json({statuscocde:200, data: result }); 
     } catch(error) { 
         console.error(error)
@@ -53,24 +53,24 @@ app.get("/add", (req,res)=>{
 });
 
 //sub
-const subtraction= (n1,n2) => {
-  return n1-n2;
+const subtraction= (num1,num2) => {
+  return num1-num2;
 }
 app.get("/subtraction", (req,res)=>{
   try{
-  const n1= parseFloat(req.query.n1);
-  const n2=parseFloat(req.query.n2);
-  if(isNaN(n1)) {
-      logger.error("n1 is incorrectly defined");
-      throw new Error("n1 incorrectly defined");
+  const num1= parseFloat(req.query.num1);
+  const num2=parseFloat(req.query.num2);
+  if(isNaN(num1)) {
+      logger.error("num1 is incorrectly defined");
+      throw new Error("num1 incorrectly defined");
   }
-  if(isNaN(n2)) {
-      logger.error("n2 is incorrectly defined");
-      throw new Error("n2 incorrectly defined");
+  if(isNaN(num2)) {
+      logger.error("num2 is incorrectly defined");
+      throw new Error("num2 incorrectly defined");
   }
   
-  logger.info('Parameters '+n1+' and '+n2+' received for subtraction');
-  const result = subtraction(n1,n2);
+  logger.info('Parameters '+num1+' and '+num2+' received for subtraction');
+  const result = subtraction(num1,num2);
   res.status(200).json({statuscocde:200, data: result }); 
   } catch(error) { 
       console.error(error)
@@ -78,49 +78,49 @@ app.get("/subtraction", (req,res)=>{
     }
 });
 //multiplication
-const multiply= (n1,n2) => {
-  return n1*n2;
+const multiply= (num1,num2) => {
+  return num1*num2;
 }
 app.get("/multiply", (req,res)=>{
   try{
-  const n1= parseFloat(req.query.n1);
-  const n2=parseFloat(req.query.n2);
-  if(isNaN(n1)) {
-      logger.error("n1 is incorrectly defined");
-      throw new Error("n1 incorrectly defined");
+  const num1= parseFloat(req.query.num1);
+  const num2=parseFloat(req.query.num2);
+  if(isNaN(num1)) {
+      logger.error("num1 is incorrectly defined");
+      throw new Error("num1 incorrectly defined");
   }
-  if(isNaN(n2)) {
-      logger.error("n2 is incorrectly defined");
-      throw new Error("n2 incorrectly defined");
+  if(isNaN(num2)) {
+      logger.error("num2 is incorrectly defined");
+      throw new Error("num2 incorrectly defined");
   }
   
-  logger.info('Parameters '+n1+' and '+n2+' received for multiplication');
-  const result = multiply(n1,n2);
+  logger.info('Parameters '+num1+' and '+num2+' received for multiplication');
+  const result = multiply(num1,num2);
   res.status(200).json({statuscocde:200, data: result }); 
   } catch(error) { 
       console.error(error)
       res.status(500).json({statuscocde:500, msg: error.toString() })
     }
 });
-//divition
-const divition= (n1,n2) => {
-  return n1/n2;
+//division
+const division= (num1,num2) => {
+  return num1/num2;
 }
-app.get("/divition", (req,res)=>{
+app.get("/division", (req,res)=>{
   try{
-  const n1= parseFloat(req.query.n1);
-  const n2=parseFloat(req.query.n2);
-  if(isNaN(n1)) {
-      logger.error("n1 is incorrectly defined");
-      throw new Error("n1 incorrectly defined");
+  const num1= parseFloat(req.query.num1);
+  const num2=parseFloat(req.query.num2);
+  if(isNaN(num1)) {
+      logger.error("num1 is incorrectly defined");
+      throw new Error("num1 incorrectly defined");
   }
-  if(isNaN(n2)) {
-      logger.error("n2 is incorrectly defined");
-      throw new Error("n2 incorrectly defined");
+  if(isNaN(num2)) {
+      logger.error("num2 is incorrectly defined");
+      throw new Error("num2 incorrectly defined");
   }
   
-  logger.info('Parameters '+n1+' and '+n2+' received for divition');
-  const result = divition(n1,n2);
+  logger.info('Parameters '+num1+' and '+num2+' received for division');
+  const result = division(num1,num2);
   res.status(200).json({statuscocde:200, data: result }); 
   } catch(error) { 
       console.error(error)
